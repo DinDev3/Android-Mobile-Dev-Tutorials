27f958a44af4a8a21c3b6b8f54ff2216cfd24a22
Dinuka <drpiyadigama@gmail.com>
2020-02-13 00:09:23 +0530

    Initiated Scrolling app project

---
+++ Week2/ScrollingText/app/src/main/java/lk/dinuka/scrollingtext/MainActivity.java
@@ -0,0 +1,20 @@
+package lk.dinuka.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+/*
+Reference:
+https://codelabs.developers.google.com/codelabs/android-training-text-and-scrolling-views/index.html?index=../..android-training#0
+ */
