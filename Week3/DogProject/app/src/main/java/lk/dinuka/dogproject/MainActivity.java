5e6cdd62ec668c38da7e7b32a8aa928691a5c942
Dinuka <drpiyadigama@gmail.com>
2020-02-20 10:06:57 +0530

    Displaying an image in LinearLayout using XML

---
+++ Week3/DogProject/app/src/main/java/lk/dinuka/dogproject/MainActivity.java
@@ -0,0 +1,25 @@
+package lk.dinuka.dogproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView v1 = findViewById(R.id.im_view);
+//
+//        String resource = "brittany_02625";
+//
+//        int resource_id = getResources().getIdentifier(resource,"drawable","lk.dinuka.dogproject");
+//        // have the project name properly in defPackage(= package). Won't work otherwise.
+//
+//        v1.setImageResource(resource_id);
+
+    }
+}
