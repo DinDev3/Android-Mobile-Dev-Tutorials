4341da8c343c21f9f9fcff88a852fd61c15f7746
Dinuka <drpiyadigama@gmail.com>
2020-02-20 08:53:49 +0530

    Added multiple items into the order

--- Week2/DroidCafeInput/app/src/main/java/lk/dinuka/droidcafeinput/MainActivity.java
+++ Week2/DroidCafeInput/app/src/main/java/lk/dinuka/droidcafeinput/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     // Tag for the intent extra.
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     // The order message, displayed in the Toast and sent to the new Activity
     private String mOrderMessage;
-//    private ArrayList<String> mAllOrderMessages = new ArrayList<String>();
+    private String mAllMessages;
+    private ArrayList<String> mAllOrderMessages = new ArrayList<String>();
 
 
     @Override
@@ -39,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
                 Intent intent = new Intent(MainActivity.this, OrderActivity.class);
-                intent.putExtra(EXTRA_MESSAGE,mOrderMessage);       // to pass one message to the Order Activity
-//                intent.putStringArrayListExtra("EXTRA_MESSAGE", mAllOrderMessages);       // to pass one message to the Order Activity
+//                intent.putExtra(EXTRA_MESSAGE,mOrderMessage);       // to pass one message to the Order Activity
+                intent.putExtra(EXTRA_MESSAGE, mAllMessages);       // to pass one message to the Order Activity
+//                intent.putStringArrayListExtra("EXTRA_MESSAGE", mAllOrderMessages);
 
                 startActivity(intent);
             }
@@ -76,20 +80,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void showDonutOrder(View view) {      // Show a message that the donut image was clicked
         mOrderMessage = getString(R.string.donut_order_message);
-//        mAllOrderMessages.add(mOrderMessage);
+        mAllOrderMessages.add(mOrderMessage);
         displayToast(getString(R.string.donut_order_message));
+
+        addAllOrdersToOneMessage();
     }
 
     public void showIceCreamOrder(View view) {      // Show a message that the ice cream sandwich image was clicked
         mOrderMessage = getString(R.string.ice_cream_order_message);
-//        mAllOrderMessages.add(mOrderMessage);
+        mAllOrderMessages.add(mOrderMessage);
         displayToast(getString(R.string.ice_cream_order_message));
+
+        addAllOrdersToOneMessage();
     }
 
     public void showFroyoOrder(View view) {      // Show a message that the froyo image was clicked
         mOrderMessage = getString(R.string.froyo_order_message);
-//        mAllOrderMessages.add(mOrderMessage);
+        mAllOrderMessages.add(mOrderMessage);
         displayToast(getString(R.string.froyo_order_message));
+
+        addAllOrdersToOneMessage();
+    }
+
+
+    public void addAllOrdersToOneMessage() {
+
+        if (mAllOrderMessages.size() != 1) {        // to make sure that the first item isn't 'null'
+            mAllMessages = mAllMessages + "\n" + mAllOrderMessages.get(mAllOrderMessages.size() - 1);      //concatenating the last order message
+        } else{
+            mAllMessages = "\n" + mAllOrderMessages.get(mAllOrderMessages.size() - 1);      //concatenating the last order message
+        }
     }
 
 }
@@ -98,4 +118,6 @@ public class MainActivity extends AppCompatActivity {
 /*
 Reference:
 https://codelabs.developers.google.com/codelabs/android-training-clickable-images/index.html?index=../../android-training#2
+
+https://codelabs.developers.google.com/codelabs/android-training-input-controls/index.html?index=../..android-training#6
  */
