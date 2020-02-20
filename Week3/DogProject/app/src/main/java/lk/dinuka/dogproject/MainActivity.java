4548a9f2de48e8580e2146d6b22b65fb7c027c5a
Dinuka <drpiyadigama@gmail.com>
2020-02-20 10:07:56 +0530

    Displaying an image using Java in ImageView

--- Week3/DogProject/app/src/main/java/lk/dinuka/dogproject/MainActivity.java
+++ Week3/DogProject/app/src/main/java/lk/dinuka/dogproject/MainActivity.java
@@ -12,14 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ImageView v1 = findViewById(R.id.im_view);
-//
-//        String resource = "brittany_02625";
-//
-//        int resource_id = getResources().getIdentifier(resource,"drawable","lk.dinuka.dogproject");
-//        // have the project name properly in defPackage(= package). Won't work otherwise.
-//
-//        v1.setImageResource(resource_id);
+        ImageView v1 = findViewById(R.id.im_view);
+
+        String resource = "brittany_02625";
+
+        int resource_id = getResources().getIdentifier(resource,"drawable","lk.dinuka.dogproject");
+        // have the project name properly in defPackage(= package). Won't work otherwise.
+
+        v1.setImageResource(resource_id);
 
     }
 }
