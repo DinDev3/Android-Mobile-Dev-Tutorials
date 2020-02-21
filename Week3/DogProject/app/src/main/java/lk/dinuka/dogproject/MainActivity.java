0753ce4b961f9915fec54a74d2d59b6f33999b0e
Dinuka <drpiyadigama@gmail.com>
2020-02-22 01:06:17 +0530

    Changing image in ImageView onClick implemented

--- Week3/DogProject/app/src/main/java/lk/dinuka/dogproject/MainActivity.java
+++ Week3/DogProject/app/src/main/java/lk/dinuka/dogproject/MainActivity.java
@@ -3,23 +3,40 @@ package lk.dinuka.dogproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView v1 = findViewById(R.id.im_view);
+
 
         String resource = "brittany_02625";
 
         int resource_id = getResources().getIdentifier(resource,"drawable","lk.dinuka.dogproject");
         // have the project name properly in defPackage(= package). Won't work otherwise.
 
+        ImageView v1 = findViewById(R.id.im_view);
         v1.setImageResource(resource_id);
 
     }
+
+    public void changeImage(View view){          // call this method onClick of Button
+        String resource = "welsh_springer_spaniel_08203";
+
+        int resource_id = getResources().getIdentifier(resource,"drawable","lk.dinuka.dogproject");
+        // have the project name properly in defPackage(= package). Won't work otherwise.
+
+        ImageView v1 = findViewById(R.id.im_view);
+
+        v1.setImageResource(resource_id);
+
+    }
+
 }
