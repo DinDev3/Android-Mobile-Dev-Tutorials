bbc2f7f1ae755c6b3c4feb3737e7050f817ddacd
Dinuka <drpiyadigama@gmail.com>
2020-02-26 20:35:32 +0530

    Implimented returning data back to the main activity

--- Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/MainActivity.java
+++ Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/MainActivity.java
@@ -2,18 +2,24 @@ package lk.dinuka.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int TEXT_REQUEST = 1;           // Request code
 
     public static final String EXTRA_MESSAGE =
             "com.example.android.twoactivities.extra.MESSAGE";
 
     private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -32,8 +40,29 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+//        the requestCode you set when you launched the Activity with startActivityForResult(),
+//        the resultCode set in the launched Activity (usually one of RESULT_OK or RESULT_CANCELED),
+//        and the Intent data that contains the data returned from the launch Activity.
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // code to test for TEXT_REQUEST to make sure you process the right Intent result,
+        // in case there are several. Also test for RESULT_OK, to make sure that the request was successful:
+        if (requestCode == TEXT_REQUEST){
+            if (resultCode == RESULT_OK){
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
 
