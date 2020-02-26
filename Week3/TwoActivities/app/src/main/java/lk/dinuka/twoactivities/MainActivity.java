9f6aa2adeec17221570ef76653afd199ea06f6aa
Dinuka <drpiyadigama@gmail.com>
2020-02-27 00:28:24 +0530

    Activity lifecycle and state tutorial implemented

--- Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/MainActivity.java
+++ Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public static final int TEXT_REQUEST = 1;           // Request code
 
     public static final String EXTRA_MESSAGE =
@@ -29,10 +30,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+        // initialize the variables
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
+
+        // restore the state
+        if (savedInstanceState != null){
+            // making sure that the Header Text of the reply screen(main screen) is visible,
+            // because if the response was received the bundle should contain reply_visible
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+
+            if (isVisible){
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));     // getting the String value that comes with the key "reply_text"
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
@@ -64,10 +120,22 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE){
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());          // reply_text is the key of the state Bundle
+        }
+    }
 }
 
 
 /*
 References:
 https://codelabs.developers.google.com/codelabs/android-training-create-an-activity/index.html?index=../..android-training#3
+
+https://codelabs.developers.google.com/codelabs/android-training-activity-lifecycle-and-state/index.html?index=../..android-training#3
  */
--- Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/SecondActivity.java
+++ Week3/TwoActivities/app/src/main/java/lk/dinuka/twoactivities/SecondActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     public static final String EXTRA_REPLY =
             "com.example.android.twoactivities.extra.REPLY";
@@ -18,6 +21,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
         mReply = findViewById(R.id.editText_second);
 
         Intent intent = getIntent();
@@ -26,12 +33,50 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
 
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
